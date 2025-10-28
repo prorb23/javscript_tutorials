b5cf2c0b13f7622af3b3464ef1c053a89e800680
Ronit Bhattacharjee <138355749+prorb23@users.noreply.github.com>
2025-10-28 07:52:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "Ronit"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//NaN
+
+//"33" => 33
+//"33abc" => NaN
+// true = 1 ; false = 0
+// null = 0
+// undefined = nan
+// string = nan
+
+let isLoggedIn = "rb"
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+//"rb" => true ; "" => false
+
+let randNo = 33
+let stringNum = String(randNo)
+console.log(stringNum);
+console.log(typeof stringNum);
