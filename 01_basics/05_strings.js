726eefffe79829d2dd369d015e891cbba525d591
Ronit Bhattacharjee <138355749+prorb23@users.noreply.github.com>
2025-10-28 18:26:01 +0000

    string basics

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "Ronit "
+const repoCount = 12
+
+// console.log(name + repoCount);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String ('RB-slayer-ronit')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('e'));
+
+const newString = gameName.substring(0,2)
+console.log(newString);
+
+const anotherString = gameName.slice(-8 , 4)
+console.log(anotherString);
+
+
+const newStringOne = "  ronit   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://rbslayer.com/ronit%20bhattacharya"
+console.log(url.replace('%20' , '-'));
+console.log(url.includes('ron'));
+
+console.log(gameName.split('-'))
+// go through the documentaion of mdn and console for more depth
