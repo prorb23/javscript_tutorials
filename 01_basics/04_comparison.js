2298492b926c3b24fa65405db535f524d4c92bf1
Ronit Bhattacharjee <138355749+prorb23@users.noreply.github.com>
2025-10-28 17:13:38 +0000

    comparison basics done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// for predictable results always use same datatypes when comparing
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+/* comparisons converts null to a number , treating it as 0,
+   thats why null >=  0 is true  */
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// strict check ===
+// console.log("2" === 2);
+
+
