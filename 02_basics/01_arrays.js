6ede6217daece95de633914bd2e43ac553c311f9
Ronit Bhattacharjee <138355749+prorb23@users.noreply.github.com>
2025-10-30 18:47:33 +0000

    array basics done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,35 @@
+// <----=======----> ARRAYS <----=======---->
+const myArr = [0 , 1 , 2 , 3 , 4 , 5 ]
+const myHeros = ["Rohit" , "Kohli" , "Messi" , "Dhoni" ]
+const myArr2 = new Array(1,2,3,4,5)
+// console.log(myArr[0]);
+
+// Array methods
+// myArr.push(8);
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// slice or splice
+console.log("A " , myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B " , myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("c " , myArr);
+console.log(myn2);
+
+
+
