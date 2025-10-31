5a1ceb1f7f26d131d05e97de058887eafb817f6b
Ronit Bhattacharjee <138355749+prorb23@users.noreply.github.com>
2025-10-31 16:20:04 +0000

    05 objects done

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,9 @@
+const course = {
+    coursename: "JS masterclass",
+    price: "999",
+    courseInstructor: "RBslayer"
+}
+const {courseInstructor: instructor} = course //destructure
+//  console.log(instructor);
+
+
