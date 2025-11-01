9be02fd7ac21efe328e178c50610abe1feda7f5a
Ronit Bhattacharjee <138355749+prorb23@users.noreply.github.com>
2025-11-01 14:27:17 +0000

    scopes overview done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,3 +10,39 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Ronit"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true){
+    const username = "Rb"
+    if(username == "Rb"){
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    //  console.log(website); // out of scope
+}
+// console.log(username); // out of scope var
+
+// ++++++++++++++++++++++ Interesting ++++++++++++++++++++++
+
+
+// console.log(addone(5))
+function  addone(num){
+    return num+1
+}
+
+// console.log(addTwo(5)) // cannot access addTwo before initialization
+const addTwo = function (num){
+    return num + 2;
+}
+// console.log(addTwo(5))
