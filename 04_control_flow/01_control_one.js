90fd0d253ef5d6796f922c51c025bec798d10393
Ronit Bhattacharjee <138355749+prorb23@users.noreply.github.com>
2025-11-02 16:36:13 +0000

    control flow done

---
+++ 04_control_flow/01_control_one.js
@@ -0,0 +1,56 @@
+//if
+const isUserLoggedIn = true
+const temp = 41
+// if(2=="2"){
+//     console.log("Executed");
+// }
+
+// if( 2 === "2" ){
+//     console.log("Executed");
+// }
+
+// if(temp === 51){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("Greater than 50");
+// }
+
+// const score = 200
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`User power : ${power}`);
+// }
+ 
+const balance = 1500
+
+// if(balance > 500) console.log("test"),console.log("test 2");
+
+// if(balance < 500){
+//     console.log("less than 500");
+// }
+// else if(balance < 750) {
+//     console.log("less than 750");
+// }
+// else if(balance < 900){
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("less than 1220");
+// }
+
+const debitCard = true
+const userLoggedIn = true
+const loggedInfromGoogle = false
+const loggedInfromEmail = true
+if(userLoggedIn && debitCard && 2==3){
+    console.log("Allowed");
+    
+}
+
+if(loggedInfromGoogle || loggedInfromEmail){
+    console.log("User Logged in");
+}
+
+ 
+// < , > , <= ,>= ,== , != ,===
