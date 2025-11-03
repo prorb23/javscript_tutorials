7449258ceb1cc988c22ccc03a73799d9bd9abfbb
Ronit Bhattacharjee <138355749+prorb23@users.noreply.github.com>
2025-11-03 16:56:20 +0000

    while and do while basics done

---
+++ 05_iterations.js/02_while_do_while.js
@@ -0,0 +1,22 @@
+// while
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is: ${index}`);
+//     index += 2
+// }
+
+let myArrY = [ "Ronit" , "RBslayer" , "RBboss"]
+
+// let i = 0
+// while (i < myArrY.length) {
+//     console.log(`Value is ${myArrY[i]}`);
+//     i++
+// }
+
+// do-while loop
+let score = 11
+
+// do {
+//     console.log(`score is ${score}`);
+//     score++   
+// } while (score <= 10);
