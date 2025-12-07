f060e14fe5826b893b8ede31960362193f757c54
Ronit Bhattacharjee <138355749+prorb23@users.noreply.github.com>
2025-12-07 05:32:08 +0000

    Promises basic concept done & Async await

---
+++ 09_advanced_one/promises.js
@@ -0,0 +1,99 @@
+const promiseOne = new Promise(function(resolve,reject){
+    //Do an async tasks
+        //DB calls , cryptography , network
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+    },1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+}) // --> Direct connection with resolve
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    },1500)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({user:"RB" , email:"RB@example.com"})
+    },1800)
+})
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false
+        if(!error){
+            resolve({username:"RB",pass:"123"})
+        }else{
+            reject('ERROR : Something went wrong!!')
+        }
+    },2000)
+})
+
+promiseFour.then((user) =>{
+    console.log(user);
+    return user.username
+})
+.then((username) => {
+    console.log(username);
+})
+.catch(function(err){
+    console.log(err);
+})
+.finally(() => console.log("Promise is either resolved or rejected")
+)
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username:"javascript",pass:"123"})
+        }else{
+            reject('ERROR : js went wrong!!')
+        }
+    },2000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+async function getAllUsers(){
+   try {
+        const response = await fetch('https://api.github.com/users/prorb23')
+        const data = await response.json()
+        console.log(data);
+   } catch (error) {
+        console.log("E: ",error);
+   }
+}
+
+// getAllUsers()
+
+fetch('https://api.github.com/users/prorb23')
+.then((response) => {
+    return response.json()
+})
+.then((response) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+
